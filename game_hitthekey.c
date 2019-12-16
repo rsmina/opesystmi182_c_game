169c248ad57ab8a0afe4f1a43d4d095431dc8c92
rsmina <smreign@gmail.com>
2019-12-16 02:09:00 +0000

    2nd commit

--- game_hitthekey.c
+++ game_hitthekey.c
@@ -16,27 +16,21 @@ int main(void) {
 
 while(1) {
     mvaddstr(10, 33, " Hit the key after the countdown!!!  ");
-    /*printf("Hit the key after the countdown!!!");*/
-    sleep(3);
-    fflush(stdout);
-
+    sleep(3000);
+    
     mvaddstr(10, 33, " 3 ");
-    /*printf(" 3 ");*/
-    sleep(1);
-    fflush(stdout);
+    sleep(1000);
 
     mvaddstr(10, 33, " 2 ");
-    /*printf(" 2 ");*/
     sleep(1);
-    fflush(stdout);
 
     mvaddstr(10, 33, " 1 ");
-    /*printf(" 1 ");*/
     sleep(1);
-    fflush(stdout);
 
 
 }
 
-
+endwin();
+return (0);
 }
+
