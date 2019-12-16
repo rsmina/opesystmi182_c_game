878606dc57265718f9b728ef243ce7846b1b8ac8
rsmina <smreign@gmail.com>
2019-12-16 03:10:24 +0000

    Added fixes

--- game_hitthekey.c
+++ game_hitthekey.c
@@ -2,9 +2,13 @@
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 int key;
 int input;
+int screenX = 0;
+int screenY = 0;
 
 /* main function */
 
@@ -12,9 +16,18 @@ int main(void) {
   initscr();
   noecho();
   curs_set(FALSE);
+  
+  cbreak();
+  keypad(stdscr, TRUE);
+  nodelay(stdscr, TRUE);
+   
+  bool windowActive = TRUE;
 
-
-while(1) {
+while(windowActive) {
+    
+    getmaxyx(stdscr, screenY, screenX);
+    int keyInput = getch(); 
+    
     mvaddstr(10, 33, " Hit the key after the countdown!!!  ");
     sleep(3000);
     
