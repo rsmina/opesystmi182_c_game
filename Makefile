ff7bb5c260be1e79b73a424726bd50c286b73a84
rsmina <smreign@gmail.com>
2019-12-19 03:22:26 +0000

    Renewed interface, fixed menu

---
+++ Makefile
@@ -0,0 +1,14 @@
+# Makefile
+
+CC = gcc
+
+LDLIBS=-lncurses
+
+SRCS := $(wildcard *.c)
+BINS := $(SRCS:%.c=%)
+
+all: ${BINS}
+
+clean:
+	@echo "Cleaning up..."
+	rm -rvf *.o ${BINS}
--- game_hitthekey.c
+++ game_hitthekey.c
@@ -1,49 +1,84 @@
-#include <ncurses.h>     // ncurses library
+#include <ncurses.h>
 #include <stdio.h>
-#include <time.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
 
-int key;
-int input;
-int screenX = 0;
-int screenY = 0;
+#define WIDTH 30
+#define HEIGHT 10 
 
-/* main function */
+int startx = 0;
+int starty = 0;
 
-int main(void) {
-  initscr();
-  noecho();
-  curs_set(FALSE);
-  
-  cbreak();
-  keypad(stdscr, TRUE);
-  nodelay(stdscr, TRUE);
-   
-  bool windowActive = TRUE;
+char *choices[] = { 
+			"Choice 1",
+			"Choice 2",
+			"Choice 3",
+			"Choice 4",
+			"Exit",
+		  };
+int n_choices = sizeof(choices) / sizeof(char *);
+void print_menu(WINDOW *menu_win, int highlight);
 
-while(windowActive) {
-    
-    getmaxyx(stdscr, screenY, screenX);
-    int keyInput = getch(); 
-    
-    mvaddstr(10, 33, " Hit the key after the countdown!!!  ");
-    sleep(3000);
-    
-    mvaddstr(10, 33, " 3 ");
-    sleep(1000);
+int main()
+{	WINDOW *menu_win;
+	int highlight = 1;
+	int choice = 0;
+	int c;
 
-    mvaddstr(10, 33, " 2 ");
-    sleep(1);
+	initscr();
+	clear();
+	noecho();
+	cbreak();
+	startx = (80 - WIDTH) / 2;
+	starty = (24 - HEIGHT) / 2;
+		
+	menu_win = newwin(HEIGHT, WIDTH, starty, startx);
+	keypad(menu_win, TRUE);
+	mvprintw(0, 0, "Use arrow keys to go up and down, Press enter to select a choice. Choose wisely");
+	refresh();
+	print_menu(menu_win, highlight);
 
-    mvaddstr(10, 33, " 1 ");
-    sleep(1);
-
+	while(1)
+	{	c = wgetch(menu_win);
+		switch(c)
+		{	case KEY_UP:
+				if(highlight == 1)
+					highlight = n_choices;
+				else
+					--highlight;
+				break;
+			case KEY_DOWN:
+				if(highlight == n_choices)
+					highlight = 1;
+				else 
+					++highlight;
+				break;
+			case 0x0A:
+				choice = highlight;
+				break;
+		}
+		print_menu(menu_win, highlight);
+		if(choice != 0)	/* User did a choice come out of the infinite loop */
+			break;
+	}	
 
 }
 
-endwin();
-return (0);
-}
 
+void print_menu(WINDOW *menu_win, int highlight)
+{
+	int x, y, i;	
+
+	x = 2;
+	y = 2;
+	box(menu_win, 0, 0);
+	for(i = 0; i < n_choices; ++i)
+	{	if(highlight == i + 1) /* High light the present choice */
+		{	wattron(menu_win, A_REVERSE); 
+			mvwprintw(menu_win, y, x, "%s", choices[i]);
+			wattroff(menu_win, A_REVERSE);
+		}
+		else
+			mvwprintw(menu_win, y, x, "%s", choices[i]);
+		++y;
+	}
+	wrefresh(menu_win);
+}
