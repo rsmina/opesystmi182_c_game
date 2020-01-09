0a4eba6497946daa82f808bc9d36b4666f932050
rsmina <smreign@gmail.com>
2020-01-09 02:00:19 +0000

    Added story path 1

--- game_hitthekey.c
+++ game_hitthekey.c
@@ -8,11 +8,11 @@ int startx = 0;
 int starty = 0;
 
 char *choices[] = { 
-			"Choice 1",
-			"Choice 2",
-			"Choice 3",
-			"Choice 4",
-			"Exit",
+			"Pick it up",
+			"Stare at it",
+			"Take a picture of it",
+			"Keep walking",
+			"[Exit Game]",
 		  };
 int n_choices = sizeof(choices) / sizeof(char *);
 void print_menu(WINDOW *menu_win, int highlight);
